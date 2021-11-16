6db2e6327f56c52cebc7aa2ee6715b093bd01919
unknown <guru090192@gmail.com>
2021-11-17 00:12:55 +0100

    refactor by django.View

--- hollymovies/hollymovies/urls.py
+++ hollymovies/hollymovies/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from viewer.views import hello_view, movies
+from viewer.views import hello_view, MoviesView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/<s0>', hello_view),
-    path('', movies),
+    path('admin/', admin.site.urls, name='a'),
+    path('hello/<s0>', hello_view,),
+    path('', MoviesView.as_view(), name='index'),
     
 ]
--- hollymovies/viewer/views.py
+++ hollymovies/viewer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from viewer.models import Movie
-
+from django.views import View
 
 def hello_view(request, s0):
     s1 = request.GET.get('s1', '')
@@ -10,10 +10,19 @@ def hello_view(request, s0):
         template_name="hello.html",
         context={'adjectives': [s0,s1, 'beatiful', 'wonderful']}
     )
+
+class MoviesView(View):
+    def get(self, request):
+        return render(
+            request, 
+            template_name='movies.html',
+            context={"movies": Movie.objects.all()}
+        )
+
     
-def movies(request):
-    return render (
-        request,
-        template_name='movies.html',
-        context={'movies': Movie.objects.all()}
-    )
+# def movies(request):
+#     return render (
+#         request,
+#         template_name='movies.html',
+#         context={'movies': Movie.objects.all()}
+#     )
